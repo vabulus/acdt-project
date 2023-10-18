2a4910f19235441aca6617c57f833b13bb541e05
Fabulus <fabio@kreisern.eu>
2023-10-18 11:45:08 +0200

    added a user constructor

--- acdt-project/Classes/User.cs
+++ acdt-project/Classes/User.cs
@@ -2,13 +2,21 @@ namespace acdt_project.Classes;
 
 public class User
 {
+    public int UserId { get; set; }
     public string Username { get; set; }
-    public int RoleID { get; set; }
-    public string phoneNumber { get; set; }
+    public int RoleId { get; set; }
+    public string PhoneNumber { get; set; }
     public string EMail { get; set; }
 
-    public int UserID { get; set; }
-
+    public User(string username, int roleId, string phoneNumber, string eMail, int userId)
+    {
+        Username = username;
+        RoleId = roleId;
+        PhoneNumber = phoneNumber;
+        EMail = eMail;
+        UserId = userId;
+    }
+    
     public void AddUser()
     {
         throw new NotImplementedException();

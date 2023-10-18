4669a7ae6ecc787d5f1ed52b5309a5f82a5f51c8
Fabulus <fabio@kreisern.eu>
2023-10-18 11:51:21 +0200

    added following functionalities: # AddIncidents # GetIncidents # ShowIncidents

--- acdt-project/Classes/Incident.cs
+++ acdt-project/Classes/Incident.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.Runtime.InteropServices;
+using acdt_project.Database;
 using acdt_project.Enums;
 using Google.Protobuf.WellKnownTypes;
 using Microsoft.EntityFrameworkCore;
@@ -8,6 +9,33 @@ namespace acdt_project.Classes;
 
 public class Incident
 {
+    
+    // if writeline a incident obj i want that all fields will get printed
+    public override string ToString()
+    {
+        return $"IncidentId: {IncidentId}\n" +
+               $"Severity: {Severity}\n" +
+               $"Status: {Status}\n" +
+               $"Cve: {Cve}\n" +
+               $"CreatedAt: {CreatedAt}\n" +
+               $"Issuer: {Issuer}\n" +
+               $"System: {System}\n" +
+               $"Description: {Description}\n";
+    }
+
+    
+    // make a constructor
+    public Incident(Severity severity, Status status, string cve, DateTime createdAt, int issuer, string system, string description)
+    {
+        Severity = severity;
+        Status = status;
+        Cve = cve;
+        CreatedAt = createdAt;
+        Issuer = issuer;
+        System = system;
+        Description = description;
+    }
+    
     [Key]
     public int IncidentId { get; set; }
     
@@ -21,23 +49,45 @@ public class Incident
     public string Description { get; set; }
 
 
-    public void AddIncident()
+    public static void AddIncident(Incident incidentObj)
+    {
+        using (var context = new IncidentContext())
+        {
+            context.Incidents.Add(incidentObj);
+            context.SaveChanges();
+        }   
+    }
+
+    public static Incident? GetIncident(int incidentId)
+    {
+        using (var context = new IncidentContext())
+        {
+            return context.Incidents.SingleOrDefault(i => i.IncidentId == incidentId);
+        }   
+    }
+
+    public static void UpdateIncident(Incident incidentToUpdate)
+    {
+        using (var context = new IncidentContext())
+        {
+            context.Incidents.Attach(incidentToUpdate);
+            context.Entry(incidentToUpdate).State = EntityState.Modified;
+            context.SaveChanges();
+        }   
+    }
+
+    public static void CloseIncident()
     {
         throw new NotImplementedException();
     }
 
-    public void EditIncident()
+    public static List<Incident> ShowIncident()
     {
-        throw new NotImplementedException();
-    }
-
-    public void CloseIncident()
-    {
-        throw new NotImplementedException();
-    }
-
-    public void ShowIncident()
-    {
-        throw new NotImplementedException();
+        using (var context = new IncidentContext())
+        {
+            // load the incidents from the database
+            var incidents = context.Incidents.ToListAsync().Result;
+            return incidents;
+        }  
     }
 }

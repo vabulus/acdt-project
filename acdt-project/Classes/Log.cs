1090463170f30d9fe40bc5100faf6e5fef6d09c0
fabio <fabio@kreisern.eu>
2023-10-23 10:40:31 +0200

    added keys for database in classes

--- acdt-project/Classes/Log.cs
+++ acdt-project/Classes/Log.cs
@@ -1,9 +1,11 @@
+using System.ComponentModel.DataAnnotations;
 using acdt_project.Enums;
 
 namespace acdt_project.Classes;
 
 public class Log
 {
+    [Key]
     public int LogId { get; set; }
     public DateTime CreatedAt { get; set; }
     public Severity LogLevel { get; set; }
